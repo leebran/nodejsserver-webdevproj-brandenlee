421b32b4f2609264d010b3f330900fd0cc319714
Branden Lee <brandenlee@Brandens-MacBook-Pro.local>
2019-06-27 00:29:20 -0400

    update api key

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const server = http.createServer((request, response) => {
     const summonerChamp = query.summonerChamp;
     const summonerCurGame = query.summonerCurGame;
     const findGame = query.findGame;
+    const sumGame = query.sumGame;
     if(summonerName != null) {
         callAPI(summonerName, response);
     } else if(summonerChamp != null){
@@ -25,6 +26,8 @@ const server = http.createServer((request, response) => {
         callAPI3(summonerCurGame, response);
     } else if (findGame != null) {
         callAPI4(findGame, response);
+    } else if (findGame != null) {
+        callAPI5(sumGame, response);
     }
     else {
         callAPI("wet dream", response)
@@ -101,6 +104,28 @@ function callAPI3(summonerChamp, response) {
 }
 function callAPI4(summonerChamp, response) {
     const url = '/lol/match/v4/matches/'
+        + summonerChamp + '?api_key=' + apiKey;
+
+    https.get(url, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            response.end(data);
+            // console.log(JSON.parse(data));
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
+function callAPI5(summonerChamp, response) {
+    const url = '/lol/match/v4/matchlists/by-account/'
         + summonerChamp + '?api_key=' + apiKey;
 
     https.get(url, (resp) => {
